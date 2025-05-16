be793de358ca1e87e77a770575070c5a56073f65
Adi Sandhu <adi.sandhu@outlook.com>
2025-05-15 21:50:47 -0600

    binary search

--- src/binary-search.cpp
+++ src/binary-search.cpp
@@ -8,7 +8,8 @@ using namespace std;
 
 int linearSearch(vector<int>& arr, int key){
     for(int i = 0; i < arr.size(); ++i){
-        if (arr[i] == key) return i;
+        if(arr[i] == key)
+            return i;
     }
     return -1;
 }
